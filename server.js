3efd540f38933874a62545a94a95517ebfb3aaeb
Muddabirchohan <muddabir22@gmail.com>
2019-05-26 14:32:13 +0500

    Update server.js

--- server.js
+++ server.js
@@ -43,6 +43,6 @@ mongoose.Promise = global.Promise;
 //     console.log("running at port 400")
 // })
 
-app.set(port,function(){
+app.listen(port,function(){
     console.log("now listening for requests");
 })
